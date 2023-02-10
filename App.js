34c7d766c7e9c88da6fcbe7db6c59199aedbb9bd
chandachewe10 <chewec03@gmail.com>
2023-02-10 15:47:55 +0000

    Added Icons in drawer for Homw,Guidance,Docs,Help,Share and Author

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import { Button, View } from 'react-native';
 import { createDrawerNavigator,DrawerContentScrollView,DrawerItemList } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeComponent from './Components/HomeComponent';
-import DrawerLogo from './Components/DrawerLogo';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { MaterialIcons } from '@expo/vector-icons';
 import DrawerContent from './Components/DrawerContent';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 
@@ -31,9 +33,47 @@ export default function App() {
             }}
      drawerContent = {(props) => <DrawerContent {...props}/>} 
       >     
-        <Drawer.Screen name="DrawerLogo" component={DrawerLogo} />
-        <Drawer.Screen name="Home" component={HomeComponent} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        
+        <Drawer.Screen name="Home" component={HomeComponent} 
+        options={{
+          title: 'Home',
+          drawerIcon: ({focused, size}) => (
+            <Ionicons name="ios-home-outline" size={24} color="black" />
+          ),
+       }}/>
+        <Drawer.Screen name="Guidance" component={NotificationsScreen} 
+       options={{
+       
+        drawerIcon: ({focused, size}) => (
+          <MaterialIcons name="assistant-direction" size={24} color="black" />
+        ),
+     }} 
+        
+        />
+        <Drawer.Screen name="UseCase" component={NotificationsScreen} 
+         options={{
+       title:'Documentation',
+          drawerIcon: ({focused, size}) => (
+            <MaterialCommunityIcons name="file-account-outline" size={24} color="black" />
+          ),
+       }} />
+       
+        <Drawer.Screen name="Help" component={NotificationsScreen} 
+         options={{
+         
+             drawerIcon: ({focused, size}) => (
+              <MaterialCommunityIcons name="help-rhombus" size={24} color="black" />
+             ),
+          }}
+        />
+        <Drawer.Screen name="Share" component={NotificationsScreen} 
+         options={{
+         
+             drawerIcon: ({focused, size}) => (
+              <MaterialIcons name="share" size={24} color="black" />
+             ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- Components/DrawerContent.js
+++ Components/DrawerContent.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Image,View, Text, ImageBackground,StyleSheet } from 'react-native';
 import { DrawerContentScrollView,DrawerItemList } from '@react-navigation/drawer';
+import styles from '../assets/AppStyles';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 function DrawerContent(props) {
     return (
-     <DrawerContentScrollView {...props}>
+        <View style={{flex:1}}>   
+     <DrawerContentScrollView {...props}> 
+     <ImageBackground source={require('../assets/AppImages/background_image.jpg')}>
+     <View style={styles.ViewLogo}>
+          <Image style={styles.AppLogo} source={require('../assets/AppImages/DrawerIcon.jpg')}/>
+        </View>
+        <Text style={{textAlign:'center',fontWeight:'600',marginTop:10,marginBottom:10,fontSize:18,color:'white'}}>Zesco Residential Calculator</Text>
+        </ImageBackground>    
+            
         <DrawerItemList {...props}/>
+
+        <View
+  style={{
+    marginTop: 10,marginBottom:10,
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  }}
+/>
+
+
+ 
+<View style={{marginLeft:10,justifyContent: 'flex-end'}}>
+<FontAwesome5 name="user-shield" size={24} color="black" />
+    <Text >Author </Text>
+ </View>
      </DrawerContentScrollView>  
+     </View>
     );
 }
 
